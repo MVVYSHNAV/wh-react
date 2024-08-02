b410c7345e5f0bca5110de9e222c40bf928f5246
Vyshanv MV <vyshnavmv15@gmail.com>
2024-08-02 09:40:00 +0530

    btn color updates dark and white change

--- wh/src/components/Button/Button.jsx
+++ wh/src/components/Button/Button.jsx
@@ -2,8 +2,10 @@ import PropTypes from 'prop-types';
 
 const Button = ({ 
   text = 'Get a Seat', 
-  bgGradientFrom = 'from-pink-600', 
+  bgGradientFrom = 'from-slate-400', 
   bgGradientTo = 'to-black', 
+  darkBgGradientFrom = 'dark:from-pink-600', 
+  darkBgGradientTo = 'dark:to-black', 
   textColor = 'text-white', 
   hoverTextColor = 'hover:text-gray-400', 
   paddingX = 'px-8', 
@@ -15,7 +17,7 @@ const Button = ({
   return (
     <div className={`relative z-20 mx-auto flex justify-center ${additionalClasses}`}>
       <button
-        className={`rounded-full bg-gradient-to-b ${bgGradientFrom} ${bgGradientTo} ${paddingX} ${paddingY} font-semibold ${textColor} ${shadow} ${hoverTextColor} drop-shadow-3xl ${opacity}`}
+        className={`rounded-full bg-gradient-to-b ${bgGradientFrom} ${bgGradientTo} ${darkBgGradientFrom} ${darkBgGradientTo} ${paddingX} ${paddingY} font-semibold ${textColor} ${shadow} ${hoverTextColor} drop-shadow-3xl ${opacity}`}
       >
         {text}
       </button>
@@ -28,6 +30,8 @@ Button.propTypes = {
   text: PropTypes.string,
   bgGradientFrom: PropTypes.string,
   bgGradientTo: PropTypes.string,
+  darkBgGradientFrom: PropTypes.string,
+  darkBgGradientTo: PropTypes.string,
   textColor: PropTypes.string,
   hoverTextColor: PropTypes.string,
   paddingX: PropTypes.string,
