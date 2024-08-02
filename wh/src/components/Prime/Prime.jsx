efcd925216d7953e82bb01f8a7221496030c4a56
Vyshanv MV <vyshnavmv15@gmail.com>
2024-08-02 22:09:06 +0530

    success section has been updated with postion and width

--- wh/src/components/Prime/Prime.jsx
+++ wh/src/components/Prime/Prime.jsx
@@ -3,12 +3,12 @@ import Successimg from '../../assets/successimg.png';
 const Prime = () => {
   return (
     <div className="h-[780px] flex items-center justify-center bg-gray-100 dark:bg-gray-950 dark:text-gray-400 md:p-16 p-9">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-1">
-        <div className="flex items-center justify-start">
-          <h1 className="sm:text-3xl text-xl font-regular font-BebasNeue text-black dark:text-white text-center md:text-left">
-            Success is not just about what <br/> you accomplish, but about the <br/>impact you make along the  way.<br/> Each 
-            challenge you face is an <br/> opportunity to grow and  each <br/>step you take brings you  closer <br/>to your goals. 
-            Embrace the journey, <br/>stay focused, and  remember that<br/> every small  effort counts towards <br/> achieving 
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+        <div className="flex items-center justify-center">
+          <h1 className="sm:text-4xl text-xl sm:w-4/5 font-regular font-BebasNeue text-black dark:text-white text-center md:text-left whitespace-normal">
+            Success is not just about what you accomplish, but about the impact you make along the  way.<span className='text-gray-400'> Each 
+            challenge you face is an  opportunity to grow and  each step you take brings you  closer to your goals.  </span> 
+            Embrace the journey, stay focused, and  remember that every small  effort counts towards  achieving 
             something great.
           </h1>
         </div>
