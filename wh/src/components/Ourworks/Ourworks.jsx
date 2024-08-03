d82f61ffd3cb70c9edb971c38e885ba04ce6c137
Vyshanv MV <vyshnavmv15@gmail.com>
2024-08-03 12:27:11 +0530

    our work base start

--- wh/src/components/Ourworks/Ourworks.jsx
+++ wh/src/components/Ourworks/Ourworks.jsx
@@ -1,7 +1,21 @@
 
 const Ourworks = () => {
   return (
-    <div>Ourworks</div>
+    <div className="relative h-[780px] overflow-hidden bg-gray-100 dark:bg-gray-950 dark:text-gray-400 md:p-16 p-9">
+      <div>
+      <h1 className="text-xl sm:text-2xl font-bold font-BebasNeue text-black dark:text-gray-400 ml-5">
+          OUR WORKS
+        </h1>
+      </div>
+      <div className="grid sm:grid-cols-12 gap-4 mt-8 w-12/12 h-[601px] p-5">
+        <div className=" bg-violet-900 sm:col-span-7">
+
+        </div>
+        <div className=" bg-violet-900 sm:col-span-5">
+
+        </div>
+      </div>
+    </div>
   )
 }
 
